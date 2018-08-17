9045be7c3ce44fd0f53113945baf19647fa79c33
Navstar Temp <navtemp@NavstarInc.Net>
2018-08-16 20:53:50 -0400

    herokuInstallation

---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+var friends = [
+  {
+    name: "Napoleon",
+    photo: "https://media.giphy.com/media/8PHvCsrkYgIEw/giphy.gif",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jon Snow",
+    photo: "https://media.giphy.com/media/has1WKhoorwLS/giphy.gif",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Rachel Green",
+    photo: "http://38.media.tumblr.com/20ad32ddb6e4a8d04dea653b0373dfa6/tumblr_mfv54rXt5l1r9zaago1_500.gif",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Ryan Gosling",
+    photo: "https://media.giphy.com/media/adgrnRDPVTIre/giphy.gif",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Mariah Carey",
+    photo: "https://media.giphy.com/media/bEbRqmAnOeRi/giphy.gif",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Oprah",
+    photo: "https://media.giphy.com/media/tZYcCUogO4dwc/giphy.gif",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// export the array
+module.exports = friends;
+
